bf32bf9c9bd6f263df136a1e9cbca5f8af2abadc
igor200023 <contatoigor950@gmail.com>
2026-04-23 16:50:27 -0300

    componentes

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,3 +1,16 @@
+import Text from "./components/text";
+
 export default function App() {
-  return <div className="text-pink-dark">Olá mundo!</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Olá mundo!
+      </Text>
+      <Text variant="body-sm-bold" className="text-green-base">
+        Olá mundo!
+      </Text>
+      <Text variant="body-md-bold">Olá mundo!</Text>
+      <Text>Levar o dog pra passear</Text>
+    </div>
+  );
 }
