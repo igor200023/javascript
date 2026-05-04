74d8ecd2a7e9e13be222230eb23d61125a522510
igor200023 <contatoigor950@gmail.com>
2026-05-04 19:07:44 -0300

    Criando o componente icon

--- todo/vite.config.ts
+++ todo/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), tailwindcss(),
+    react(), tailwindcss(), svgr(),
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
