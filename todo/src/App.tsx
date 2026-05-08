52e98daa0425690586ca9659e18c3f57520a1782
igor200023 <contatoigor950@gmail.com>
2026-05-08 15:07:15 -0300

    button incon feito, usando hover

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -8,6 +8,7 @@ import XIcon from "./assets/icons/x.svg?react";
 import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
 export default function App() {
   return (
 
@@ -39,6 +40,12 @@ export default function App() {
 
       <div>
         <Button icon={PlusIcon}>Nova tarefa</Button>
+      </div>
+
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon} />
+        <ButtonIcon icon={TrashIcon} variant="secondary" />
+        <ButtonIcon icon={TrashIcon} variant="tertiary" />
       </div>
     </div>
   );
