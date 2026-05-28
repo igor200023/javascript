603627f6542d7bf81026ce2510442118de74bf49
igor200023 <contatoigor950@gmail.com>
2026-05-27 22:58:29 -0300

    Base montada

--- todo/src/pages/page-home.tsx
+++ todo/src/pages/page-home.tsx
@@ -1,13 +1,15 @@
 import Container from "../components/container";
-import TaskItem from "../core-components/task-item";
+import TasksList from "../core-components/tasks-list";
 import TasksSummary from "../core-components/tasks-summary";
 
 export default function PageHome() {
-    return <Container as="article" className="space-y-3">
+    return (
+    <Container as="article" className="space-y-3">
         <header className="flex items-center justify-between">
             <TasksSummary />
         </header>
 
-        <TaskItem />
-    </Container>;
+        <TasksList />
+    </Container>
+    );
 }
